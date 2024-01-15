8074a7bf533119d49471cd34d9cf03f4cf39c4cb
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-15 22:58:54 +0000

    moved timer and question functions into a startQuiz function

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -15,60 +15,66 @@ var newQuestion3 = document.getElementById('c');
 var newQuestion4 = document.getElementById('d');
 var rightOrWrong = document.getElementById('wrongOrRight');
 var timeEl = document.querySelector(".timer");
-var secondsLeft = 0;
 
 
 
-
-
-function fetchData() {
-fetch('./assets/script/questions.js')
-.then(response => response.text())
-.then(data => {
-    newQuestion(data, 0);
-    chosenAnswer(data, 0);
-    console.log(data);
-})
-.catch(err => {
-    console.error(err);
-});
-}
-//questions array has been fetched from questions.js
 
 
 //function needed for timer to start on quiz start
-startButton.addEventListener('click', () => {
-    fetchData();
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz (){
+    var timeInterval;
+    var currentQuestionsIndex = 0;
+    
+    var secondsLeft = 0;
     var timerInterval = setInterval (function() {
         secondsLeft++;
         timeEl.textContent = secondsLeft; 
         
-        },1000)})
+        },1000)
+    
+    newQuestion(currentQuestionsIndex);
+    }
 
 
 //function needed to change questions
-function newQuestion (data, i){
-// Event.preventDefault();
-newQuestionTitle.textContent = data[i].title;
-newQuestion1.textContent = (data[i].choices[0]);
-newQuestion2.textContent = (data[i].choices[1]);
-newQuestion3.textContent = (data[i].choices[2]);
-newQuestion4.textContent = (data[i].choices[3]);
+function newQuestion (questionIndex){
+var currentQuestion = questions[questionIndex];
+newQuestionTitle.textContent = currentQuestion.title;
+newQuestion1.textContent = currentQuestion.choices[0];
+newQuestion2.textContent = currentQuestion.choices[1];
+newQuestion3.textContent = currentQuestion.choices[2];
+newQuestion4.textContent = currentQuestion.choices[3];
+
+newQuestion1.addEventListener('click', function() {
+    chosenAnswer(currentQuestion, 0);
+  });
+  newQuestion2.addEventListener('click', function() {
+    chosenAnswer(currentQuestion, 1);
+  });
+  newQuestion3.addEventListener('click', function() {
+    chosenAnswer(currentQuestion, 2);
+  });
+  newQuestion4.addEventListener('click', function() {
+    chosenAnswer(currentQuestion, 3);
+  });
 }
 
 
+
 //function to check the chosen answer
-function chosenAnswer (){
-    for (let i = 0; i < questions.length; i++) {
-        var answer = document.getElementById('choices').children;
-        if (answer[i].innerHTML === questions[i].answer) {
+function chosenAnswer (questions, choiceIndex){
+        if (question.correctAnswerIndex === choiceIndex) {
         rightOrWrong.textContent = "Correct!";  
         } else {
         rightOrWrong.textContent = "Incorrect! Try Again!";
         return;
         }
 }
-}
+
+// Inside the function, it checks if the correctAnswerIndex of the current question (referred to as question) is equal to the choiceIndex passed as an argument. If they are equal, it sets the text content of an element called rightOrWrong to "Correct!". 
+// If the correctAnswerIndex and choiceIndex are not equal, it sets the text content of rightOrWrong to "Incorrect! Try Again!" and then returns from the function.
 
 
 
@@ -88,3 +94,7 @@ function storeTime() {
 
 //code needed to replace button text with link to highscores.html
 //addEventListener('click',)
+
+function endQuiz() {
+    // Display the final score and allow the user to save their initials and score
+  }
