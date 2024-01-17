e0525a43eea3b5898ab0e42425c835af480c73c8
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-17 17:46:43 +0000

    simplified code with TA

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -9,75 +9,98 @@
 
 var startButton = document.getElementById('start');
 var newQuestionTitle =  document.getElementById('question-title');
-var newQuestion1 = document.getElementById('a');
-var newQuestion2 = document.getElementById('b');
-var newQuestion3 = document.getElementById('c');
-var newQuestion4 = document.getElementById('d');
+// var newQuestion1 = document.getElementById('a');
+// var newQuestion2 = document.getElementById('b');
+// var newQuestion3 = document.getElementById('c');
+// var newQuestion4 = document.getElementById('d');
+var choicesEl = document.getElementById('choices');
 var rightOrWrong = document.getElementById('wrongOrRight');
 var timeEl = document.getElementById('time');
-const hiddenStartScreen = document.querySelectorAll('.hide0');
-const hiddenQuestions = document.querySelectorAll('.hide1');
-const hiddenEndQuiz = document.querySelectorAll('.hide2');
-const hiddenHighScores = document.querySelectorAll('.hide3');
+var questionsEl = document.getElementById('questions');
+// const hiddenStartScreen = document.querySelectorAll('.hide0');
+// const hiddenQuestions = document.querySelectorAll('.hide1');
+// const hiddenEndQuiz = document.querySelectorAll('.hide2');
+// const hiddenHighScores = document.querySelectorAll('.hide3');
 
 var timeInterval;
+var questionIndex = 0;
+var secondsTaken = 0;
 startButton.addEventListener('click', startQuiz);
 
+//hide visual intro div, unide question div and start clock and go to question function
 function startQuiz (){
-  hiddenStartScreen.forEach(function(element) {
-    element.style.display = 'none';
-  });
-  hiddenQuestions.forEach(function(element) {
-    element.style.display = 'block';
-  });
-    var currentQuestionsIndex = 0;
+  var startScreenEl =  document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
+
+questionsEl.removeAttribute('class');
+  // hiddenStartScreen.forEach(function(element) {
+  //   element.style.display = 'none';
+  // });
+  // hiddenQuestions.forEach(function(element) {
+  //   element.style.display = 'block';
+  // });
+    
     //code needed for timer to start on quiz start
-    var secondsTaken = 0;
-    var timerInterval = setInterval (function() {
+    
+        timerInterval = setInterval (function() {
         secondsTaken++;
         timeEl.textContent = secondsTaken; 
         },1000);
     
-    newQuestion(currentQuestionsIndex);
-    currentQuestionsIndex++;
+    newQuestion();
+    // currentQuestionsIndex++;
     //not changing to next question
     }
 
-
+//bring in questions and options and reuse function
 //function needed to change questions
-function newQuestion (questionIndex){
+function newQuestion (){
 var currentQuestion = questions[questionIndex];
 newQuestionTitle.textContent = currentQuestion.title;
-newQuestion1.textContent = currentQuestion.choices[0];
-newQuestion2.textContent = currentQuestion.choices[1];
-newQuestion3.textContent = currentQuestion.choices[2];
-newQuestion4.textContent = currentQuestion.choices[3];
-
-newQuestion1.addEventListener('click', function() {
-    chosenAnswer(currentQuestion, 0);
-  });
-  newQuestion2.addEventListener('click', function() {
-    chosenAnswer(currentQuestion, 1);
-  });
-  newQuestion3.addEventListener('click', function() {
-    chosenAnswer(currentQuestion, 2);
-  });
-  newQuestion4.addEventListener('click', function() {
-    chosenAnswer(currentQuestion, 3);
-  });
+console.log(currentQuestion.choices.length);
+// newQuestion1.textContent = currentQuestion.choices[0];
+// newQuestion2.textContent = currentQuestion.choices[1];
+// newQuestion3.textContent = currentQuestion.choices[2];
+// newQuestion4.textContent = currentQuestion.choices[3];
+choicesEl.innerHTML = "";
+for (let i = 0; i < currentQuestion.choices.length; i++) {
+  var choice = currentQuestion.choices[i];
+  var btnEl = document.createElement('button');
+  btnEl.setAttribute('class', 'choice');
+  btnEl.setAttribute('value', choice);
+  btnEl.textContent = i + 1 + '. ' + choice;
+  btnEl.onclick = chosenAnswer;
+  choicesEl.appendChild(btnEl);
+}
+// newQuestion1.addEventListener('click', function() {
+//     chosenAnswer(currentQuestion, 0);
+//   });
+//   newQuestion2.addEventListener('click', function() {
+//     chosenAnswer(currentQuestion, 1);
+//   });
+//   newQuestion3.addEventListener('click', function() {
+//     chosenAnswer(currentQuestion, 2);
+//   });
+//   newQuestion4.addEventListener('click', function() {
+//     chosenAnswer(currentQuestion, 3);
+//   });
 }
 
 
 
-//function to check if the chosen answer matches the correct answer
-function chosenAnswer (question, choiceIndex){
-        while (question.correctAnswerIndex !== choiceIndex){
-        rightOrWrong.textContent = "Incorrect! Try Again!";
-        return;
-        }
-        rightOrWrong.textContent = "Correct!";  
-        currentQuestionIndex++;
-        newQuestion(currentQuestionsIndex);
+//function to check if the chosen answer is wrong, apply penalty
+function chosenAnswer (){
+  //1.check if answer is wrong, update time - add penalty
+  //2. questionIndex ++
+  //3. if you've gone over 100sec or is question Index = question array
+  //if you are out of questions -- end of game else go to next question
+        // while (question.correctAnswerIndex !== choiceIndex){
+        // rightOrWrong.textContent = "Incorrect! Try Again!";
+        // return;
+        // }
+        // rightOrWrong.textContent = "Correct!";  
+        // currentQuestionIndex++;
+        // newQuestion(currentQuestionsIndex);
         } 
         
 //currently displaying incorrect but not letting player resubmit
