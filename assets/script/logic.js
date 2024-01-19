38c30b50403754a474683963c8c16608224815e6
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-19 11:34:42 +0000

    started coding quizEnd,highScore and goToHighScore functions

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -15,6 +15,8 @@ var choicesEl = document.getElementById('choices');
 var timeEl = document.getElementById('time');
 var questionsEl = document.getElementById('questions');
 var endScreenEl = document.getElementById('end-screen');
+var sumbitEL = document.getElementById('submit');
+var initialsEL = document.getElementById('initials');
 
 var timeInterval;
 var questionIndex = 0;
@@ -39,16 +41,15 @@ questionsEl.removeAttribute('class');
     
     newQuestion();
     // currentQuestionsIndex++;
-    //not changing to next question
     }
 
-//bring in questions and options and reuse function
 
+//bring in questions and options and reuse function
 function newQuestion (){
   var currentQuestion = questions[questionIndex];
   console.log(currentQuestion.question);
 newQuestionTitle.textContent = currentQuestion.question;
-
+//isn't showing up past first question
 
 choicesEl.innerHTML = "";
 // for (let i = 0; i < currentQuestion.choices.length; i++) {
@@ -73,8 +74,8 @@ currentQuestion.choices.forEach(function (choice, i) {
 //function to check if the chosen answer is wrong, apply penalty
 function chosenAnswer (){
   //write the entire array path out -- this was the major issue with the code was carrying the data - the shortcut of the array
-  console.log(currentQuestion);
-  var correctAnswer = currentQuestion.answer;
+  // console.log(currentQuestion);
+  // var correctAnswer = currentQuestion.answer;
   
   if (this.value !== questions[questionIndex].answer){
     //changed correctAnswer to questions[questionIndex].answer
@@ -118,24 +119,44 @@ function chosenAnswer (){
 // If the correctAnswerIndex and choiceIndex are not equal, it sets the text content of rightOrWrong to "Incorrect! Try Again!" and then returns from the function.
 
 
-var highScores;
-
 
 //Display the final score and allow the user to save their initials and score
 function quizEnd (){
   console.log("you have reached the quizEnd function!") //added console log
+  clearInterval(timerInterval);
+  timeEl.textContent = secondsTaken;
+
+questionsEl.setAttribute('class', 'hide');
+endScreenEl.removeAttribute('class');
   //stop the timer by applying clearInterval and update time on the DOM
   //hide current screen and unhide the score screen
 
 }
 
+var initials;
 //event listener for the input of initials button
+sumbitEL.addEventListener('click', function(event){
+  event.preventDefault();
+  initials = initialsEL.value.trim();
+  localStorage.setItem('initials', initials);
+});
+
+
 
 //display score and getItem and setItem of LS
 function highScore() {
-//on quiz end, store time and initials in local storage
-//addEventListener('click', submit)
-// var newHighScore = localStorage.setItem('highScores', JSON.stringify(highScores));
+  
+if (initials !== "") {
+  var newHighScore = localStorage.getItem ('initials');
+}
+var tempData = {
+  score: 100,
+  time: 60,
+  initials: 'AB'
+};
+tempData.push(newHighScore);
+localStorage.setItem('tempdata', JSON.stringify(tempData));
+location.href = 'highscores.html';
 
 //get the value of the input box
   //set a rule that if input box is not an empty string, withing the rule/conditional the logic should be as follows
--- assets/script/scores.js
+++ assets/script/scores.js
@@ -1,6 +1,16 @@
 
 //function to lead to highscores after pressing submit button
+var highScoreEL = document.getElementById('highscores');
+
 function goToHighScores(){
+    var highScores = localStorage.getItem('highScores');
+    for (let i = 0; i < highScores.length; i++) {
+        const hS = highScores[i];
+        var newScore = document.createElement ('li');
+        li.textContent = hS;
+        highScoreEL.appendChild(li);
+    }
+
     // localStorage.getItem('highScores');
     //you are pulling an array.  In order to display an entire array you mush use a for loop.
 }
